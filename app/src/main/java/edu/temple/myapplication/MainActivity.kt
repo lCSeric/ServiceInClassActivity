3ed61d9a18e7d27509857e3cedbb8a97bacf2a83
Eric Lin <tur99299@temple.edu>
2024-04-09 12:05:01 -0400

    Initialize the timeBinder and variable isConnected.
    use bindService or handler to bind the TimeService and set the start, pause, stop with isConnected

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,58 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import java.security.KeyStore.TrustedCertificateEntry
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    val handler = Handler(Looper.getMainLooper()){
+        true
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
 
+        val serviceConnection = object : ServiceConnection{
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                timerBinder = service as TimerService.TimerBinder
+                timerBinder.isRunning
+                isConnected = true
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                isConnected = false
+            }
+
+        }
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) timerBinder.start(100, handler)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerBinder.pause()
         }
-        
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnected) timerBinder.stop()
         }
+        bindService(
+            Intent(this,TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
     }
 }
--- app/src/main/java/edu/temple/myapplication/TimerService.kt
+++ app/src/main/java/edu/temple/myapplication/TimerService.kt
@@ -3,6 +3,7 @@ package edu.temple.myapplication
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Handler
 import android.os.IBinder
 import android.util.Log
 
@@ -15,6 +16,8 @@ class TimerService : Service() {
 
     private var paused = false
 
+    private var handler : Handler? = null
+
     inner class TimerBinder : Binder() {
 
         // Check if Timer is already running
@@ -22,8 +25,16 @@ class TimerService : Service() {
             get() = this@TimerService.isRunning
             set(value) {this@TimerService.isRunning = value}
 
-        // Start a new timer
         fun start(startValue: Int){
+            start(startValue, null)
+        }
+
+        // Start a new timer
+        fun start(startValue: Int, timeHandler: Handler?){
+
+            timeHandler?.run {
+                handler = timeHandler
+            }
 
             if (!paused) {
                 if (!isRunning) {
@@ -78,7 +89,7 @@ class TimerService : Service() {
             try {
                 for (i in startValue downTo 1)  {
                     Log.d("Countdown", i.toString())
-
+                    handler?.sendEmptyMessage(i)
                         while (paused);
                         sleep(1000)
 
